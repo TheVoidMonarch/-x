fb2b6cede9d29ad4529b7461488870160f563dca
TheVoidMonarch <112571254+TheVoidMonarch@users.noreply.github.com>
2024-06-06 00:30:25 +0800

    libft- 42 School

--- ft_bzero.c
+++ ft_bzero.c
@@ -1,52 +1,11 @@
 #include <stdlib.h>
 #include <unistd.h>
 
-void    ft_bzero(void *s, size_t n)
+void ft_bzero(void *s, size_t n)
 {
     unsigned char *ptr = (unsigned char *)s;
-    while (n-- );
+    while (n--)
     {
-        s[i]++;
+        *ptr++ = 0;
     }
-}
-    #include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <unistd.h>
-
-// Declaration of ft_bzero
-void ft_bzero(void *s, size_t n);
-s = 0;
-
-int main()
-{
-    // Allocate a buffer and initialize it with some values
-    size_t size = 10;
-    unsigned char buffer[size];
-
-    // Fill the buffer with some values
-    for (size_t i = 0; i < size; i++) {
-        buffer[i] = i + 1; // 1, 2, 3, ..., 10
-    }
-
-    // Print buffer before ft_bzero
-    printf("Buffer before ft_bzero:\n");
-    for (size_t i = 0; i < size; i++) {
-        printf("%d ", buffer[i]);
-    }
-    printf("\n");
-
-    // Call ft_bzero to set the buffer to zero
-    ft_bzero(buffer, size);
-
-    // Print buffer after ft_bzero
-    printf("Buffer after ft_bzero:\n");
-    for (size_t i = 0; i < size; i++) {
-        printf("%d ", buffer[i]);
-    }
-    printf("\n");
-
-    return 0;
-}
-    unsigned char *ptr = (unsigned char *)s;
 }
