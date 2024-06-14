bf3b74ec91a4c4e61b53b895d7921a451b231572
Void <112571254+TheVoidMonarch@users.noreply.github.com>
2024-06-15 04:46:43 +0800

    Update ft_bzero.c

--- ft_bzero.c
+++ ft_bzero.c
@@ -1,10 +1,11 @@
 #include <stdlib.h>
 #include <unistd.h>
-
+/*#include <stdio.h>*/
 void ft_bzero(void *s, size_t n)
 {
-    unsigned char *ptr = (unsigned char *)s;
-    while (--n)
+    unsigned char   *ptr;
+    ptr = (unsigned char *)s;
+    while (n--)
     {
         *ptr++ = 0;
     }
