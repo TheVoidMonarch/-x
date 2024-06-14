393093755413d9aeba9ab1d30fe2865f7dcff19c
Void <112571254+TheVoidMonarch@users.noreply.github.com>
2024-06-15 04:54:01 +0800

    Update ft_strncmp.c

---
+++ ft_strncmp.c
@@ -0,0 +1,17 @@
+#include <unistd.h>
+
+int	ft_strncmp(char *s1, char *s2, unsigned int n)
+{
+    if (s1 == NULL || s2 == NULL)
+        return (404);
+    if (n == 0)
+        return (0);
+	unsigned int	i;
+
+	i = 0;
+	while (s1[i] && s2[i] && s1[i] == s2[i] && i < n - 1)
+		i++;
+	if ((i == n - 1) || (s1[i] == '\0' && s2[i] == '\0'))
+		return (0);
+	return ((unsigned char)s1[i] - (unsigned char)s2[i]);
+}
