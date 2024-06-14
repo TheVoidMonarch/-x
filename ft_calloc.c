0432cbb830152fa846eab19fd15ad36182dfa9b5
Void <112571254+TheVoidMonarch@users.noreply.github.com>
2024-06-15 04:46:20 +0800

    Update ft_calloc.c

--- ft_calloc.c
+++ ft_calloc.c
@@ -2,20 +2,17 @@
 #include <stdlib.h>
 
 void	*ft_calloc(size_t count, size_t size)
-}
 {
-
-	void *ptr;
-	size_t i;
-	unsigned char *temp_ptr;
+	void	*ptr;
+	size_t	i;
+	unsigned char	*temp_ptr;
+	temp_ptr = (unsigned char *) ptr;
 
 	ptr = malloc(count * size);
 	if (!ptr)
 		return (NULL);
-
-	temp_ptr = (unsigned char *) ptr;
 	i = 0;
-	while ( i < count * size)
+	while (i < count * size)
 	{
 		temp_ptr[i] = 0;
 		i++;
